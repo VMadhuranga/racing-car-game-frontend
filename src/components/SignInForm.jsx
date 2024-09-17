ee163ff49cbbe8328b882d77ab60f75b82165fc9
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2024-09-17 16:58:01 +0530

    Update SignInForm component

--- src/components/SignInForm.jsx
+++ src/components/SignInForm.jsx
@@ -1,13 +1,33 @@
-import { Link } from "react-router-dom";
-import UserForm from "./UserForm";
+import { Form, Link, useActionData } from "react-router-dom";
 
 export default function SignInForm() {
+  const errors = useActionData();
+
   return (
-    <div className="sign-in-form">
-      <UserForm buttonContent={"Sign in"} />
+    <section className="sign-in-form">
+      <h2>Sign in</h2>
+      <Form method="POST">
+        <div>
+          <label htmlFor="username">User name:</label>
+          <input type="text" id="username" name="username" required />
+          {errors &&
+            errors.username?.map((error, index) => (
+              <span key={index}>{"* " + error}</span>
+            ))}
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input type="text" id="password" name="password" required />
+          {errors &&
+            errors.password?.map((error, index) => (
+              <span key={index}>{"* " + error}</span>
+            ))}
+        </div>
+        <button type="submit">Sign in</button>
+      </Form>
       <p>
         Don&apos;t have an account? <Link to={"/sign-up"}>Sign up</Link>
       </p>
-    </div>
+    </section>
   );
 }
--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -1,13 +1,46 @@
-import { Link } from "react-router-dom";
-import UserForm from "./UserForm";
+import { Form, Link, useActionData } from "react-router-dom";
 
 export default function SignUpForm() {
+  const errors = useActionData();
+
   return (
-    <div className="sign-up-form">
-      <UserForm buttonContent={"Sign up"}></UserForm>
+    <section className="sign-up-form">
+      <h2>Sign up</h2>
+      <Form method="POST">
+        <div>
+          <label htmlFor="username">User name:</label>
+          <input type="text" id="username" name="username" required />
+          {errors &&
+            errors.username?.map((error, index) => (
+              <span key={index}>{"* " + error}</span>
+            ))}
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input type="text" id="password" name="password" required />
+          {errors &&
+            errors.password?.map((error, index) => (
+              <span key={index}>{"* " + error}</span>
+            ))}
+        </div>
+        <div>
+          <label htmlFor="confirm-password">Confirm password:</label>
+          <input
+            type="text"
+            id="confirm-password"
+            name="confirm-password"
+            required
+          />
+          {errors &&
+            errors["confirm-password"]?.map((error, index) => (
+              <span key={index}>{"* " + error}</span>
+            ))}
+        </div>
+        <button type="submit">Sign up</button>
+      </Form>
       <p>
         Already have an account? <Link to={"/sign-in"}>Sign in</Link>
       </p>
-    </div>
+    </section>
   );
 }
