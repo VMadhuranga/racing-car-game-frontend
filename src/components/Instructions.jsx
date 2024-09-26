e6471cd57949a72514268fd272df4a25601c70f9
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2024-09-26 09:03:23 +0530

    Create Instructions component

---
+++ src/components/Instructions.jsx
@@ -0,0 +1,28 @@
+import { Link, useParams } from "react-router-dom";
+
+export default function Instructions() {
+  const { userId } = useParams();
+
+  return (
+    <section className="instructions">
+      <h2>Instructions</h2>
+      <ul>
+        <li>
+          Press <kbd>Arrow key right</kbd> to go to right
+        </li>
+        <li>
+          Press <kbd>Arrow key left</kbd> to go to left
+        </li>
+        <li>
+          Press <kbd>Shift</kbd> + <kbd>Arrow key right</kbd> to go to right
+          faster
+        </li>
+        <li>
+          Press <kbd>Shift</kbd> + <kbd>Arrow key left</kbd> to go to left
+          faster
+        </li>
+      </ul>
+      <Link to={`/${userId}`}>{"<-"} Go back</Link>
+    </section>
+  );
+}
