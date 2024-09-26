2eaa7f1fee68cc6146ce90c98befcd2b6320deea
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2024-09-26 11:38:05 +0530

    Update refresh function

--- src/actions/refresh.js
+++ src/actions/refresh.js
@@ -4,15 +4,19 @@ import setAccessToken from "../utils/set-access-token";
 import getAccessToken from "../utils/get-access-token";
 
 export default async function refresh(baseUrl) {
-  const token = getAccessToken();
+  try {
+    const accessToken = getAccessToken();
 
-  if (!token || decodeJwt(token).exp < Date.now()) {
-    const response = await axios.get(`${baseUrl}/refresh`, {
-      withCredentials: true,
-    });
+    if (!accessToken || decodeJwt(accessToken).exp < Date.now()) {
+      const response = await axios.get(`${baseUrl}/refresh`, {
+        withCredentials: true,
+      });
 
-    const { accessToken, userId } = response.data;
-    setAccessToken(accessToken);
-    return userId;
+      const { accessToken, userId } = response.data;
+      setAccessToken(accessToken);
+      return userId;
+    }
+  } catch {
+    return null;
   }
 }
