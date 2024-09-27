5db3d3d604feac2f069f8bd5d8981ec44cb256ad
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2024-09-27 08:52:19 +0530

    Render LoadingIndicator component according to navigation state

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
-import { Outlet, useLocation } from "react-router-dom";
+import { Outlet, useLocation, useNavigation } from "react-router-dom";
+import LoadingIndicator from "./components/LoadingIndicator";
 
 function App() {
   const location = useLocation();
+  const navigation = useNavigation();
 
   return (
     <>
       {!location.pathname.includes("play") && <h1>Racing Car Game</h1>}
+      {(navigation.state === "loading" ||
+        navigation.state === "submitting") && <LoadingIndicator />}
       <Outlet />
     </>
   );
