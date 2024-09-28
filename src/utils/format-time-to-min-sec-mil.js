006b5857a4b3099bf39930fa0676dc6f442611de
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2024-09-28 09:30:30 +0530

    Update formatTimeToMinSecMil function

--- src/utils/format-time-to-min-sec-mil.js
+++ src/utils/format-time-to-min-sec-mil.js
@@ -2,5 +2,5 @@ export default function formatTimeToMinSecMil(time) {
   const mil = Math.floor(time % 1000);
   const sec = Math.floor((time / 1000) % 60);
   const min = Math.floor((time / 60000) % 60);
-  return `${min}:${sec}:${mil}`;
+  return `${min.toString().padStart(2, "0")}:${sec.toString().padStart(2, "0")}:${mil.toString().padStart(3, "0")}`;
 }
