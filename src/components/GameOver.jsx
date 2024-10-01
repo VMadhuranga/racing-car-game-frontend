c857f662f158a3bafa0e3e119f991f24f91b2902
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2024-10-01 16:09:20 +0530

    Create handleRestartClick and handleExitClick functions

--- src/components/GameOver.jsx
+++ src/components/GameOver.jsx
@@ -1,8 +1,25 @@
 import { createPortal } from "react-dom";
 import formatTimeToMinSecMil from "../utils/format-time-to-min-sec-mil";
-import { Form } from "react-router-dom";
+import { Form, useNavigate, useOutletContext } from "react-router-dom";
 
 export function GameOver({ time, handleRestart }) {
+  const user = useOutletContext();
+  const navigate = useNavigate();
+
+  function handleRestartClick(e) {
+    if (Number(time) <= Number(user["best-time"])) {
+      e.preventDefault();
+      handleRestart();
+    }
+  }
+
+  function handleExitClick(e) {
+    if (Number(time) <= Number(user["best-time"])) {
+      e.preventDefault();
+      navigate(`/${user.id}`);
+    }
+  }
+
   return createPortal(
     <section
       style={{
@@ -21,11 +38,15 @@ export function GameOver({ time, handleRestart }) {
       <p>{formatTimeToMinSecMil(time)}</p>
       <Form method="PATCH" onSubmit={handleRestart}>
         <input type="hidden" name="best-time" value={time} />
-        <button type="submit">Restart</button>
+        <button type="submit" onClick={handleRestartClick}>
+          Restart
+        </button>
       </Form>
       <Form method="PATCH" action="exit">
         <input type="hidden" name="best-time" value={time} />
-        <button type="submit">Exit</button>
+        <button type="submit" onClick={handleExitClick}>
+          Exit
+        </button>
       </Form>
     </section>,
     document.getElementById("root"),
