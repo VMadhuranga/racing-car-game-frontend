52090387ef8cb6ba6bb198a023f61958ae802cfe
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2024-10-01 16:11:17 +0530

    Update browser router

--- src/main.jsx
+++ src/main.jsx
@@ -8,14 +8,16 @@ import {
 import App from "./App.jsx";
 import SignInForm from "./components/SignInForm.jsx";
 import SignUpForm from "./components/SignUpForm.jsx";
-import signUp from "./actions/sign-up.js";
 import MainMenu from "./components/MainMenu.jsx";
+import Menu from "./components/Menu.jsx";
 import Profile from "./components/Profile.jsx";
 import EditProfile from "./components/EditProfile.jsx";
 import LeaderBoard from "./components/LeaderBoard.jsx";
 import GamePlay from "./components/GamePlay.jsx";
 import Instructions from "./components/Instructions.jsx";
 import ErrorPage from "./components/ErrorPage.jsx";
+import LoadingIndicator from "./components/LoadingIndicator.jsx";
+import signUp from "./actions/sign-up.js";
 import signIn from "./actions/sign-in.js";
 import getUser from "./loaders/get-user.js";
 import updateUsername from "./actions/update-username.js";
@@ -26,7 +28,6 @@ import signOut from "./loaders/sign-out.js";
 import updateBestTime from "./actions/update-best-time.js";
 import getLeaderBoard from "./loaders/get-leader-board.js";
 import "./main.css";
-import LoadingIndicator from "./components/LoadingIndicator.jsx";
 
 const baseUrl = import.meta.env.VITE_BACKEND_BASE_URL;
 
@@ -99,111 +100,117 @@ const router = createBrowserRouter([
         loader: async ({ params }) => {
           await refresh(baseUrl);
           const { userId } = params;
-          await getUser(baseUrl, userId);
-          return null;
-        },
-      },
-      {
-        path: "/:userId/play",
-        element: <GamePlay />,
-        action: async ({ params, request }) => {
-          await refresh(baseUrl);
-          const { userId } = params;
-
-          const formData = Object.fromEntries(
-            (await request.formData()).entries(),
-          );
-
-          await updateBestTime(baseUrl, userId, formData);
-          return null;
-        },
-      },
-      {
-        path: "/:userId/play/exit",
-        action: async ({ params, request }) => {
-          await refresh(baseUrl);
-          const { userId } = params;
-
-          const formData = Object.fromEntries(
-            (await request.formData()).entries(),
-          );
-
-          await updateBestTime(baseUrl, userId, formData);
-          return redirect(`/${userId}`);
-        },
-      },
-      {
-        path: "/:userId/instructions",
-        element: <Instructions />,
-      },
-      {
-        path: "/:userId/leader-board",
-        element: <LeaderBoard />,
-        loader: async ({ params }) => {
-          await refresh(baseUrl);
-          const { userId } = params;
-          const leaderBoard = await getLeaderBoard(baseUrl, userId);
-          return leaderBoard;
-        },
-      },
-      {
-        path: "/:userId/profile",
-        element: <Profile />,
-      },
-      {
-        path: "/:userId/profile/edit",
-        element: <EditProfile />,
-        loader: async ({ params }) => {
-          await refresh(baseUrl);
-          const { userId } = params;
           const user = await getUser(baseUrl, userId);
           return user;
         },
-        action: async ({ params, request }) => {
-          await refresh(baseUrl);
-          const { userId } = params;
+        children: [
+          {
+            index: true,
+            element: <Menu />,
+          },
+          {
+            path: "play",
+            element: <GamePlay />,
+            action: async ({ params, request }) => {
+              await refresh(baseUrl);
+              const { userId } = params;
 
-          const formData = Object.fromEntries(
-            (await request.formData()).entries(),
-          );
+              const formData = Object.fromEntries(
+                (await request.formData()).entries(),
+              );
 
-          if (Object.hasOwn(formData, "new-username")) {
-            const errors = await updateUsername(baseUrl, userId, formData);
+              await updateBestTime(baseUrl, userId, formData);
+              return null;
+            },
+          },
+          {
+            path: "play/exit",
+            action: async ({ params, request }) => {
+              await refresh(baseUrl);
+              const { userId } = params;
 
-            if (errors) {
-              return { errors };
-            }
+              const formData = Object.fromEntries(
+                (await request.formData()).entries(),
+              );
 
-            const isUsernameUpdated = true;
-            return { isUsernameUpdated };
-          }
+              await updateBestTime(baseUrl, userId, formData);
+              return redirect(`/${userId}`);
+            },
+          },
+          {
+            path: "instructions",
+            element: <Instructions />,
+          },
+          {
+            path: "leader-board",
+            element: <LeaderBoard />,
+            loader: async ({ params }) => {
+              await refresh(baseUrl);
+              const { userId } = params;
+              const leaderBoard = await getLeaderBoard(baseUrl, userId);
+              return leaderBoard;
+            },
+          },
+          {
+            path: "profile",
+            element: <Profile />,
+          },
+          {
+            path: "profile/edit",
+            element: <EditProfile />,
+            loader: async ({ params }) => {
+              await refresh(baseUrl);
+              const { userId } = params;
+              const user = await getUser(baseUrl, userId);
+              return user;
+            },
+            action: async ({ params, request }) => {
+              await refresh(baseUrl);
+              const { userId } = params;
 
-          if (
-            Object.hasOwn(formData, "old-password") &&
-            Object.hasOwn(formData, "new-password") &&
-            Object.hasOwn(formData, "confirm-new-password")
-          ) {
-            const errors = await updatePassword(baseUrl, userId, formData);
+              const formData = Object.fromEntries(
+                (await request.formData()).entries(),
+              );
 
-            if (errors) {
-              return { errors };
-            }
+              if (Object.hasOwn(formData, "new-username")) {
+                const errors = await updateUsername(baseUrl, userId, formData);
 
-            const isPasswordUpdated = true;
-            return { isPasswordUpdated };
-          }
+                if (errors) {
+                  return { errors };
+                }
 
-          return null;
-        },
-      },
-      {
-        path: "/:userId/profile/delete",
-        loader: async ({ params }) => {
-          await refresh(baseUrl);
-          const { userId } = params;
-          await deleteUser(baseUrl, userId);
-          return redirect("/");
-        },
+                const isUsernameUpdated = true;
+                return { isUsernameUpdated };
+              }
+
+              if (
+                Object.hasOwn(formData, "old-password") &&
+                Object.hasOwn(formData, "new-password") &&
+                Object.hasOwn(formData, "confirm-new-password")
+              ) {
+                const errors = await updatePassword(baseUrl, userId, formData);
+
+                if (errors) {
+                  return { errors };
+                }
+
+                const isPasswordUpdated = true;
+                return { isPasswordUpdated };
+              }
+
+              return null;
+            },
+          },
+          {
+            path: "profile/delete",
+            loader: async ({ params }) => {
+              await refresh(baseUrl);
+              const { userId } = params;
+              await deleteUser(baseUrl, userId);
+              return redirect("/");
+            },
+          },
+        ],
       },
     ],
   },
