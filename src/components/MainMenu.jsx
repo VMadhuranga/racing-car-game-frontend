2e0a7971ca83ae98251e25d2b531c403d00df20d
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2024-10-01 16:10:56 +0530

    Update MainMenu component

--- src/components/MainMenu.jsx
+++ src/components/MainMenu.jsx
@@ -1,23 +1,6 @@
-import { Link } from "react-router-dom";
+import { Outlet, useLoaderData } from "react-router-dom";
 
 export default function MainMenu() {
-  return (
-    <section className="main-menu">
-      <h2>Main menu</h2>
-      <ul>
-        <li>
-          <Link to={"play"}>Start game</Link>
-        </li>
-        <li>
-          <Link to={"instructions"}>Instructions</Link>
-        </li>
-        <li>
-          <Link to={"leader-board"}>Leader board</Link>
-        </li>
-        <li>
-          <Link to={"profile"}>Profile</Link>
-        </li>
-      </ul>
-    </section>
-  );
+  const user = useLoaderData();
+  return <Outlet context={user} />;
 }
