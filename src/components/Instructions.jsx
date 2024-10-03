a206113487f73e2cb81455075c201295211d3e7e
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2024-10-03 08:11:55 +0530

    Add touch screen instructions

--- src/components/Instructions.jsx
+++ src/components/Instructions.jsx
@@ -4,8 +4,9 @@ export default function Instructions() {
   const { userId } = useParams();
 
   return (
-    <section>
+    <section className="instructions">
       <h2>Instructions</h2>
+      <p>On Keyboard:</p>
       <ul>
         <li>
           Press <kbd>Arrow key right</kbd> to go to right
@@ -21,6 +22,12 @@ export default function Instructions() {
           Press <kbd>Shift</kbd> + <kbd>Arrow key left</kbd> to go to left
           faster
         </li>
+      </ul>
+      <hr />
+      <p>On Touch Screen:</p>
+      <ul>
+        <li>Touch right side of the track to go to right</li>
+        <li>Touch left side of the track to go to left</li>
       </ul>
       <Link to={`/${userId}`}>{"<-"} Go back</Link>
     </section>
